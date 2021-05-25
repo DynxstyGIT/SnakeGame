88dcefdc09cd068d131c23031405804342ceee12
Dynxsty <dynxsty@javadiscord.net>
2021-05-26 00:24:21 +0200

    Add files via upload

---
+++ SnakeGame/src/main/java/Snake.java
@@ -0,0 +1,58 @@
+import javax.swing.*;
+import java.awt.*;
+import java.io.File;
+import java.util.concurrent.TimeUnit;
+
+
+public class Snake extends JFrame {
+
+    public static boolean sound = true;
+    public static JCheckBoxMenuItem i1;
+
+    public Snake() throws InterruptedException {
+
+        File f = new File("ciphertext");
+        if (!f.exists()) {
+            Highscore.write(0);
+
+            TimeUnit.SECONDS.sleep(3);
+
+            initUI();
+        } else { initUI(); }
+    }
+
+    private void initUI() {
+
+        try {
+            add(new Game());
+        } catch (Exception e) { e.printStackTrace(); }
+
+        setResizable(false);
+        pack();
+
+        setTitle("Snake");
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        ImageIcon imageIcon = new ImageIcon("src/resources/images/snakegameicon.png");
+        setIconImage(imageIcon.getImage());
+
+            JMenuBar mb = new JMenuBar();
+            JMenu menu = new JMenu("Options");
+            i1 = new JCheckBoxMenuItem("Sound", sound);
+            menu.add(i1);
+            mb.add(menu);
+            setJMenuBar(mb);
+            setVisible(true);
+        }
+
+    public static void main(String[] args) {
+
+        EventQueue.invokeLater(() -> {
+            JFrame ex = null;
+            try { ex = new Snake(); }
+            catch (InterruptedException e) { e.printStackTrace(); }
+
+            ex.setVisible(true);
+        });
+    }
+}
